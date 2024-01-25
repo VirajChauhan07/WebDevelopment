1c834dc03ea2f0907c88bc4ce335de73b1339094
viraj chauhan <virajj583@gmail.com>
2024-01-25 08:02:20 +0530

    Javascript oneshot covered

--- project-of-WD/BookStore/productsAPI/src/db/conn.js
+++ project-of-WD/BookStore/productsAPI/src/db/conn.js
@@ -1,18 +1,18 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
 async function connectDB() {
   try {
     await mongoose.connect(
-      "mongodb+srv://virajj583:79vlKCf9pCx30jk0@cluster0.qt6qwez.mongodb.net/",
+      'mongodb+srv://virajj583:79vlKCf9pCx30jk0@cluster0.qt6qwez.mongodb.net/',
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
       }
-    );
-    console.log("Connected to MongoDB");
+    )
+    console.log('Connected to MongoDB')
   } catch (err) {
-    console.log("Error in Connecting", err);
+    console.log('Error in Connecting', err)
   }
 }
 
-module.exports = connectDB;
+module.exports = connectDB
